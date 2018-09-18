e693eb4017bd24ecf7d73b2fcac477e68dbc2cd0
alin684 <al684@cornell.edu>
2018-09-18 11:52:31 -0400

    ta

--- main.js
+++ main.js
@@ -94,3 +94,17 @@ function pushIssue (id) {
 
   fetchIssues();
 }
+
+function turnAroundIssue (id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for(var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.push(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
