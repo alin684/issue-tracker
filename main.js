6e34325f3fdfdc4a89206e5be8fc48abc868a7e5
alin684 <al684@cornell.edu>
2018-09-24 21:22:05 -0400

    remove delete

--- main.js
+++ main.js
@@ -66,17 +66,3 @@ function setStatusClosed (id) {
 
   fetchIssues();
 }
-
-function deleteIssue (id) {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-
-  for(var i = 0; i < issues.length; i++) {
-    if (issues[i].id == id) {
-      issues.splice(i, 1);
-    }
-  }
-
-  localStorage.setItem('issues', JSON.stringify(issues));
-
-  fetchIssues();
-}
