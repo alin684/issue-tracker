0810aa9667b3267fbfbe3c466e758342cedb1e4a
alin684 <al684@cornell.edu>
2018-10-08 21:53:14 -0400

    testing still

--- main.js
+++ main.js
@@ -57,20 +57,5 @@ function testIssues () {
   var issuesList = document.getElementById('issuesList');
 
   issuesList.innerHTML = '';
-
-  for (var i = 0; i < issues.length; i++) {
-    var id = issues[i].id;
-
-    var status = issues[i].status;
-
-    issuesList.innerHTML +=   '<div class="well">'+
-                              '<h6>Issue ID: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
-                              '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
-                              '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
-                              '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
-                              '</div>';
   }
 }
