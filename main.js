5efa611e9dfbf2d6a1202b225363811a4576226a
alin684 <al684@cornell.edu>
2018-11-02 15:42:17 -0400

    rework build

--- main.js
+++ main.js
@@ -77,18 +77,6 @@ function buildIssue(e) {
 }
 
 function buildIssue(e) {
-  var issueId = chance.guid();
-  var issueDesc = document.getElementById('issueDescInput').value;
-  var issueSeverity = document.getElementById('issueSeverityInput').value;
-  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  var issueStatus = 'Open';
-  var issue = {
-    id: issueId,
-    description: issueDesc,
-    severity: issueSeverity,
-    assignedTo: issueAssignedTo,
-    status: issueStatus
-  }
 
   if (localStorage.getItem('issues') === null) {
     var issues = [];
